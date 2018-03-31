90786bd29ed7e005f634e2dfd4b8e0fd03bb521b
Clint <cdunn6754@gmail.com>
2018-03-31 12:33:57 -0600

    Restart Repo

--- personal_site/homeApp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url, include
-from django.contrib import admin
-
-from homeApp.views import IndexView
-urlpatterns = [
-    url(r'^', IndexView.as_view(), name='index')
-]
